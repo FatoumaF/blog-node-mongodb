3ab68656e20c8809672b1285c4eaf98622ef151a
Fatouma fofana <fatouma53@live.fr>
2023-11-04 18:46:56 +0100

    organisation fichier

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,37 @@ app.get('/', (req, res)=>{
 app.get('/:id', (req, res)=>{
     const {id} = req.params
  const one = data.find(el => el.id === parseInt(id))
- if(!one) return res.json({message : 'not found'})
+ if(!one) return res.status(404).json({message : 'not found'})
 res.status(200).json(one)
 })
 // POST recupérer donner envoyer par le client
-app.post('/',(req, send)=>{
-const client = req.body
-console.log(body)
+app.post('/',(req, res)=>{
+const {body} = req
+const newOne = {
+    id : data.length + 1,
+    ...body
+}
+data.push(newOne)
+res.status(201).json(newOne)
+})
+
+//upadate modifié
+app.put('/:id', (req, res)=>{
+    const {id} = req.params
+    const { body} = req
+    const one = data.find(el => el.id === parseInt(id))
+    if(!one) return res.status(404).json({message : 'not found'})
+    one.langage = body.langage
+    res.status(200).json(one)
+
+})
+
+app.delete('/:id', (req, res)=>{
+    const {id} = req.params
+    const one = data.find(el => el.id === parseInt(id))
+    if(!one) return res.status(404).json({message : 'not found'})
+    data.splice(data.indexOf(id), 1)
+    res.status(200).json({message :"ref supprimée"})
 
 })
 app.listen(port, () => {
